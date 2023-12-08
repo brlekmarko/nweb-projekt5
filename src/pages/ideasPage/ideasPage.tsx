f2cda32fdd9b9270d531f46213994cb718bac69f
Marko Brlek <marko.brlek@fer.hr>
2023-12-08 17:33:52 +0100

    Implemented caching and bg sync

--- src/pages/ideasPage/ideasPage.tsx
+++ src/pages/ideasPage/ideasPage.tsx
@@ -9,30 +9,30 @@ const IdeasPage = () => {
 
                 <div className="ryan-gossling">
                     <div className="idea">
-                        <img src="/bw1.jpg" alt="idea" />
+                        <img src="https://i.pinimg.com/originals/be/1a/dd/be1add52a129e10ecb8283e62d3413d2.jpg" alt="idea" />
                     </div>
                     <div className="idea">
-                        <img src="/bw2.jpg" alt="idea" />
+                        <img src="https://w0.peakpx.com/wallpaper/1001/382/HD-wallpaper-ryan-gosling-hey-girl-i-m-home-hollywood-oscars-ryan-gosling-black-and-white-actor.jpg" alt="idea" />
                     </div>
                 </div>
                 
                 <div className="taxi-driver">
                     <div className="idea">
-                        <img src="/bw3.jpg" alt="idea" />
+                        <img src="https://api.time.com/wp-content/uploads/2016/02/taxi-driver.jpeg" alt="idea" />
                     </div>
                     <div className="idea">
-                        <img src="/bw4.jpg" alt="idea" />
+                        <img src="https://people.com/thmb/M587Bge2X5gk_5pjDOYzpp5v5ng=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(511x0:513x2)/taxi-driver-1024-c17ee860e8e24dfcbe257c2ef8d05194.jpg" alt="idea" />
                     </div>
                 </div>
 
                 <div className="fight-club">
 
                     <div className="idea">
-                        <img src="/bw5.jpg" alt="idea" />
+                        <img src="https://miro.medium.com/v2/resize:fit:475/1*sKdz6jOdHuax1oPugpam3g.jpeg" alt="idea" />
                     </div>
 
                     <div className="idea">
-                        <img src="/bw6.jpg" alt="idea" />
+                        <img src="https://i.pinimg.com/474x/28/a3/da/28a3daa4fac278f67d1d9972ba4646c1.jpg" alt="idea" />
                     </div>
 
                 </div>
@@ -40,11 +40,11 @@ const IdeasPage = () => {
                 <div className="american-psycho">
 
                     <div className="idea">
-                        <img src="/bw7.jpg" alt="idea" />
+                        <img src="https://i1.sndcdn.com/artworks-i1HipgDlHrjOHipm-taM4Fg-t500x500.jpg" alt="idea" />
                     </div>
 
                     <div className="idea">
-                        <img src="/bw8.jpg" alt="idea" />
+                        <img src="https://i.pinimg.com/564x/63/1a/66/631a663da9bd4b52422b953db91ccfe5.jpg" alt="idea" />
                     </div>
 
                 </div>
